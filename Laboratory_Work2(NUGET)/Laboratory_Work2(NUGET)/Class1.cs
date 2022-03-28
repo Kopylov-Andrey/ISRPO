81cc437b67e9170bdc9efc8612a840452d7fdd40
Andrey Kopylov <andrey.kopylov.03@bk.ru>
2022-03-29 01:28:45 +0300

    first commit

---
+++ Laboratory_Work2(NUGET)/Laboratory_Work2(NUGET)/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Work2_NUGET_
+{
+    public class Logger
+    {
+        public void Log(string text, float number)
+        {
+            string strnum = number.ToString();
+            Console.WriteLine(text + strnum);
+        }
+
+    }
+}
