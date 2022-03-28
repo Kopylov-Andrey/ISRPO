8cae82d19bfd0b4ee39cc7318ab9bc2adf526f46
Andrey Kopylov <andrey.kopylov.03@bk.ru>
2022-03-29 01:39:49 +0300

    second commit

---
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Laboratory_Work2_NUGET_;
+
+namespace ConsoleAppTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Laboratory_Work2_NUGET_.Logger log = new Logger();
+
+            log.Log("sdg", 5);
+        }
+    }
+}
